82cff641d9639c557dc14698436812cef7b26a36
Gentlesammy <samodunlade84@gmail.com>
2022-03-18 23:38:22 +0100

    swagger controller for login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import {Body, Controller, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/authDto';
 import { accessToken } from './interface/user.interface';
@@ -8,7 +9,10 @@ export class AuthController {
 
     constructor(private authService: AuthService){}
 
+
     @Post('login')
+    @ApiOkResponse({description: "accessToken generated"})
+    @ApiUnauthorizedResponse({description: "Invalid or no credentials"})
     login (@Body() dto: AuthDto):accessToken {
         return this.authService.signinLocal(dto)
     }
