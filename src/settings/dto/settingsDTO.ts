bd0bc1f3d4fe2c41c1fd7ff3294ec6d6d076be2f
Gentlesammy <samodunlade84@gmail.com>
2022-03-24 21:28:13 +0100

    swagger updated for settings

--- src/settings/dto/settingsDTO.ts
+++ src/settings/dto/settingsDTO.ts
@@ -1,9 +1,11 @@
 
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
+
 export class SettingsDTO{
-    
     @IsString()
+    @ApiProperty({type: String, description: "settings title"})
     readonly settings_title: string;
 }
 
--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiBody, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { SettingsDTO } from './dto/settingsDTO';
 import { Setting } from './interface/settingsInterface';
 import { SettingsService } from './settings.service';
@@ -11,6 +12,8 @@ export class SettingsController {
 // get all settings 
 @UseGuards(AuthGuard('jwt'))
 @Get()
+@ApiUnauthorizedResponse({description : "invalid credentials"})
+@ApiBearerAuth()
     async getAllSettings():Promise<Setting[]>{
         return await this.settingsService.getAllSettings()
     }
@@ -18,6 +21,8 @@ export class SettingsController {
 // get a specific setting
 @UseGuards(AuthGuard('jwt'))
 @Get(':title')
+@ApiUnauthorizedResponse({description : "invalid credentials"})
+@ApiBearerAuth()
     async getASettings(@Param('title') title):Promise<Setting>{
         return await this.settingsService.getASettings(title)
     }
@@ -25,14 +30,18 @@ export class SettingsController {
 //create a setting 
 @UseGuards(AuthGuard('jwt'))
 @Post()
+@ApiUnauthorizedResponse({description : "invalid credentials"})
+@ApiBody({type: SettingsDTO})
+@ApiBearerAuth()
     async createASetting(@Body() body: SettingsDTO):Promise<Setting>{
         return await this.settingsService.createSetting(body);
     }
 
 //delete a setting
 @UseGuards(AuthGuard('jwt'))
-
+@ApiUnauthorizedResponse({description : "invalid credentials"})
 @Delete(":title")
+@ApiBearerAuth()
     deleteAsetting(@Param('title') title):Promise<Setting>{
         return this.settingsService.delete(title)
     }
@@ -40,6 +49,7 @@ export class SettingsController {
 
 //edit a setting 
 // @UseGuards(AuthGuard('jwt'))
+// @ApiBody({type: SettingsDTO})
 // @Put(":title")
 //     async editASetting(@Body() settingDto: SettingsDTO, @Param('title') title):Promise<Setting> {
 //         return this.settingsService.update(title, settingDto.settings_title)
