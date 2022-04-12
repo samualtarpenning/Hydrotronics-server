bc3f92ec8760f52ca577fe63e8bda998abc0a56b
Gentlesammy <samodunlade84@gmail.com>
2022-04-12 19:52:56 +0100

    zones interface added

--- src/zones/interface/zonesInterface.ts
+++ src/zones/interface/zonesInterface.ts
@@ -1,14 +1,41 @@
-export interface Setting{
-    id? : string;
-    settings_title: string;
-    settings_values: [SubSetting?];
-    created_at?: Date;
-    updatedAt?: Date;
+export interface Light{
+    pin_id : number;
+    name: string;
+    status_on : boolean;
+    updatedAt: Date;
 }
 
-export interface SubSetting{
+export interface Pump{
+    pin_id : number;
+    name: string;
+    status_on : boolean;
+    updatedAt: Date;
+}
+
+export interface Fan{
+    pin_id : number;
+    name: string;
+    status_on : boolean;
+    updatedAt: Date;
+}
+
+export interface Misc{
+    pin_id : number;
+    name: string;
+    status_on : boolean;
+    updatedAt: Date;
+}
+
+
+export interface Zone{
     id? : string;
-    duration: Number
+    name: string;
+    temperature: number;
+    humidity: number;
+    light: {Light};
+    pump: {Pump};
+    fan: {Pump};
+    misc: {Misc};
     createdAt? : Date;
     updatedAt? : Date;
 }
